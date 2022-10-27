7ac0df35f4a78c290be37f06f48814add9d2e798
Robert Bittick <robertbittick@gmail.com>
2022-10-26 22:23:19 -0700

    commit tree ae5395b5fa6aa4b83aab1d744aa755d7fb691f5b

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  const reverseWord = reverseString(word)
+  return word === reverseWord
 }
 
+function reverseString(word) {
+  return word.split('').reverse().join('')
+}
 /* 
   Add your pseudocode here
+  take a string, 
+  check if it is a palindrome
+  return true if true
+  return false if false
+
 */
 
 /*
   Add written explanation of your solution here
+  The code take a string, turns it into an array splitting each letter into an element, reverses the order of the elements and joins them back together. The code then takes this new string and compares it to the orignal string and if the two strings are the same it returns true, if they are no the same, it return false.
 */
 
 // You can run `node index.js` to view these console logs
